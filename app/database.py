a44714bd40444a46752ebb22da4483f24e0c498b
jjessieshang <17jessies@gmail.com>
2022-08-03 09:54:27 -0600

    add row

--- app/database.py
+++ app/database.py
@@ -1,13 +1,15 @@
+from re import L, T
 from typing import Type
 from numpy import deprecate_with_doc
 from app import app
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
+from wtforms.validators import DataRequired, InputRequired, Length, Regexp, NumberRange
+from datetime import date
 
 # the name of the database; add path if necessary
 # db_name = 'ats.sqlite'
@@ -46,6 +48,18 @@ class Directory(db.Model):
     Longitude = db.Column(db.Float)
     stress = db.Column(db.String)
 
+    def __init__(self, Directory, LSD, SC, TWP, RG, W, M, adjusted_UWI, Latitude, Longitude, stress):
+        self.Directory = Directory
+        self.LSD = LSD
+        self.SC = SC
+        self.TWP = TWP
+        self. RG = RG
+        self.W = W
+        self.M = M
+        self.adjusted_UWI = adjusted_UWI
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+
 class Properties(db.Model):
     __tablename__ = 'wellProperties'
     Directory = db.Column(db.String, primary_key=True)
@@ -55,7 +69,7 @@ class Properties(db.Model):
     AdditionalNotes = db.Column(db.String)
     Shmin = db.Column(db.String)
     Shmax = db.Column(db.String)
-    Verticalstress = db.Column(db.String)
+    VerticalStress = db.Column(db.String)
     Temperature = db.Column(db.String)
     PorePressure = db.Column(db.String)
     YoungsModulus = db.Column(db.String)
@@ -67,31 +81,102 @@ class Properties(db.Model):
     pWave = db.Column(db.String)
     sWave = db.Column(db.String)
 
-class ReadForm(FlaskForm):
-    name = StringField('Which actor is your favorite?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    def __init__(self, Directory, Depth, Type, Description, AdditionalNotes, Shmin, Shmax, VerticalStress, Temperature,
+                PorePressure, YoungsModulus, ShearModulus, BulkModulus, PoissonsRatio, CohesiveStrength, FrictionAngle, pWave, sWave):
+        self.Directory = Directory
+        self.Depth = Depth
+        self.Type = Type
+        self.Description = Description
+        self.AdditionalNotes = AdditionalNotes
+        self.Shmin = Shmin
+        self.Shmax = Shmax
+        self.VerticalStress = VerticalStress
+        self.Temperature = Temperature
+        self.PorePressure = PorePressure
+        self.YoungsModulus = YoungsModulus
+        self.ShearModulus = ShearModulus
+        self.BulkModulus = BulkModulus
+        self.PoissonsRatio = PoissonsRatio
+        self.CohesiveStrength = CohesiveStrength
+        self.FrictionAngle =FrictionAngle
+        self.pWave = pWave
+        self.sWave = sWave
 
+class AddRecord(FlaskForm):
+    # id used only by update/edit
+    id_field = HiddenField()
+    Directory = StringField('Well Name', [ InputRequired()])
+    Depth = StringField('Depth')
+    Type = StringField('Rock Type')
+    Description = StringField('Description')
+    AdditionalNotes = StringField('Notes')
+    Shmin = StringField('Minimum horizontal stress')
+    Shmax = StringField('Maximum horizontal stress')
+    VerticalStress = StringField('Vertical stress')
+    Temperature = StringField('Temperature')
+    PorePressure = StringField('Pore Pressure')
+    YoungsModulus = StringField('Young\'s Modulus')
+    ShearModulus = StringField('Shear Modulus')
+    BulkModulus = StringField('Bulk Modulus')
+    PoissonsRatio = StringField('Poisson\'s ratio')
+    CohesiveStrength = StringField('Cohesive Strength')
+    FrictionAngle = StringField('Friction Angle')
+    pWave = StringField('p Wave')
+    sWave = StringField('s Wave')
 
-@app.route('/database/')
-def read():
-    # try:
-    #     uwi = Directory.query.all()
-    #     uwi_text = '<ul>'
-    #     for well in uwi:
-    #         uwi_text += '<li>' + well.adjusted_UWI + ', ' + well.Directory + '</li>'
-    #     uwi_text += '</ul>'
-    #     return uwi_text
-    # except Exception as e:
-    #     # e holds description of the error
-    #     error_text = "<p>The error:<br>" + str(e) + "</p>"
-    #     hed = '<h1>Something is broken.</h1>'
-    #     return hed + error_text
-        # get a list of unique values in the style column
-    try:
-        uwi = Directory.query.all()
-        return render_template('database.html', uwi=uwi)
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+    # updated - date - handled in the route function
+    updated = HiddenField()
+    submit = SubmitField('Add/Update Record')
+
+@app.route('/add_record', methods=['GET', 'POST'])
+def add_record():
+    form1 = AddRecord()
+    if form1.validate_on_submit():
+        Directory = request.form['Directory']
+        Depth = request.form['Depth']
+        Type = request.form['Type']
+        Description = request.form['Description']
+        AdditionalNotes = request.form['AdditionalNotes']
+        Shmin = request.form['Shmin']
+        Shmax = request.form['Shmax']
+        VerticalStress = request.form['VerticalStress']
+        Temperature = request.form['Temperature']
+        PorePressure = request.form['PorePressure']
+        YoungsModulus = request.form['YoungsModulus']
+        ShearModulus = request.form['ShearModulus']
+        BulkModulus = request.form['BulkModulus']
+        PoissonsRatio = request.form['PoissonsRatio']
+        CohesiveStrength = request.form['CohesiveStrength']
+        FrictionAngle = request.form['FrictionAngle']
+        pWave = request.form['pWave']
+        sWave = request.form['sWave']
+
+        # the data to be inserted into Sock model - the table, socks
+        record = Properties(Directory, Depth, Type, Description, AdditionalNotes, Shmin, Shmax, VerticalStress, Temperature,
+                PorePressure, YoungsModulus, ShearModulus, BulkModulus, PoissonsRatio, CohesiveStrength, FrictionAngle, pWave, sWave)
+        # Flask-SQLAlchemy magic adds record to database
+        db.session.add(record)
+        db.session.commit()
+        # create a message to send to the template
+        message = f"The data for well {Directory} has been submitted."
+        return render_template('add_record.html', message=message)
+    else:
+        for field, errors in form1.errors.items():
+            for error in errors:
+                flash("Error in {}: {}".format(
+                    getattr(form1, field).label.text,
+                    error
+                ), 'error')
+        return render_template('add_record.html', form1=form1)
+
+# @app.route('/database/')
+#     def read():
+#         #Printing the data table
+#         try:
+#             uwi = Properties.query.all()
+#             return render_template('database.html', uwi=uwi)
+#         except Exception as e:
+#             # e holds description of the error
+#             error_text = "<p>The error:<br>" + str(e) + "</p>"
+#             hed = '<h1>Something is broken.</h1>'
+#     return hed + error_text
